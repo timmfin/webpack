e40d97836a0c1d5b144f96984d7d866463c8c988
Tim Finley <tfinley@hubspot.com>
2016-06-07 22:27:13 -0400

    Customer resolver that works with DllReferencePlugin
    To prevent the original source from being having to be present for
    things to work (because relative required paths are still resolved on
    the filesystem even though the source of the file/module is never used).

--- examples/dll-user/webpack.config.js
+++ examples/dll-user/webpack.config.js
@@ -9,6 +9,109 @@ module.exports = {
 		new webpack.DllReferencePlugin({
 			scope: "beta",
 			manifest: require("../dll/js/beta-manifest.json")
-		})
+		}),
+
+		{
+			apply: function(compiler) {
+
+				// compiler.plugin("compile", function(params) {
+
+				// 	params.normalModuleFactory.apply({
+				// 		apply: function(normalModuleFactory) {
+				// 			normalModuleFactory.plugin("factory", function(factory) {
+				// 				return function (data, callback) {
+				// 					console.log("custom module factory", data);
+
+				// 					if (false && data.request === '../dll/alpha') {
+				// 						var DelegatedModule = require("./DelegatedModule");
+				// 						callback(null, new DelegatedModule(this.options.source, resolved, this.options.type, requestPlusExt));
+				// 					} else {
+				// 						factory(data, callback);
+				// 					}
+				// 				}
+				// 			});
+				// 		}
+				// 	});
+				// });
+
+
+				// Copied from NormalModule
+				function contextify(options, request) {
+					return request.split("!").map(function(r) {
+						var rp = path.relative(options.context, r);
+						if(path.sep === "\\")
+							rp = rp.replace(/\\/g, "/");
+						if(rp.indexOf("../") !== 0)
+							rp = "./" + rp;
+						return rp;
+					}).join("!");
+				}
+
+
+				compiler.resolvers['normal'].apply({
+					apply: function(resolver) {
+
+						console.log("compiler", compiler);
+
+						if (compiler.options.plugins) {
+							var dllReferencePluginOptions = [];
+
+							compiler.options.plugins.forEach(function(plugin) {
+								if (plugin.constructor.name === 'DllReferencePlugin') {
+									dllReferencePluginOptions.push(plugin.options)
+								}
+							});
+
+						}
+
+						resolver.plugin('resolve', function(request, resolveCallback) {
+							// if (request.request === '../dll/alpha') {
+							// }
+							console.log("request in custom resolver", request);
+
+							for (var i = 0; i < dllReferencePluginOptions.length; i++) {
+								var pluginOptions = dllReferencePluginOptions[i];
+								var manifestContext = pluginOptions.context;
+								var manifestContent = pluginOptions.manifest && pluginOptions.manifest.content;
+								var manifestName    = pluginOptions.manifest && pluginOptions.manifest.name;
+
+								if (manifestContext) {
+									var fullRequest = path.resolve(request.path, request.request);
+
+									// Get these from elsewhere? (the DelegatedModuleFactoryPlugin
+									// has them as options to pass in with these defaults)
+									var extensionsToTry = ['', '.js'];
+
+									for (var j = 0; j < extensionsToTry.length; j++) {
+										var fullRequestWithExt = fullRequest + extensionsToTry[j];
+										console.log("fullRequestWithExt", fullRequestWithExt);
+
+										var relativeToManifest = contextify(pluginOptions, fullRequestWithExt);
+
+										console.log("relativeToManifest", relativeToManifest);
+
+										if (relativeToManifest && manifestContent && relativeToManifest in manifestContent) {
+											return resolver.doResolve('resolved', {
+												request: relativeToManifest,
+												resource: fullRequestWithExt,
+												path: fullRequestWithExt,
+											}, 'custom resolver ' + request.request, resolveCallback);
+										}
+
+									}
+
+								}
+							}
+
+							resolveCallback();
+						});
+
+					}
+				});
+
+
+
+			}
+		}
 	]
 };
